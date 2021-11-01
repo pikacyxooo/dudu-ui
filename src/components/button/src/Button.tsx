f9e8fd33901394e098aa263514416864dd55719c
modiarts <73455837+modiarts@users.noreply.github.com>
2021-11-01 18:50:32 +0800

    update

---
+++ src/components/button/src/Button.tsx
@@ -0,0 +1,13 @@
+import {
+    defineComponent
+} from 'vue'
+
+const Button = defineComponent({
+    render() {
+        return (
+            <button>this is button</button>
+        )
+    }
+})
+
+export default Button;
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+// import { DuButton } from './components/button'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
